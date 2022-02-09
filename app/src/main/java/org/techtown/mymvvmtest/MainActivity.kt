a43c03314bec6487557d1e4857c82b3fc74c2c99
Olio <jgg0819@naver.com>
2022-02-09 23:46:48 +0900

    초기 메인화면 구성
    - 하단 버튼 추가
    - 프래그먼트 유저화면으로 보여주기 및 프래그먼트 두개 생성

--- app/src/main/java/org/techtown/mymvvmtest/MainActivity.kt
+++ app/src/main/java/org/techtown/mymvvmtest/MainActivity.kt
@@ -4,8 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    //유저화면, 즐겨찾기 화면.
+    private lateinit var userFragment: UserFragment
+    private lateinit var myFavoritesFragment: MyFavoritesFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initSet()
+    }
+
+    //초기 설정.
+    private fun initSet(){
+        userFragment = UserFragment()
+        myFavoritesFragment = MyFavoritesFragment()
+
+        //처음엔 유저 화면 보여줌.
+        supportFragmentManager.beginTransaction().replace(R.id.cl_container, userFragment).commit()
     }
 }
---
+++ app/src/main/java/org/techtown/mymvvmtest/UserFragment.kt
@@ -0,0 +1,34 @@
+package org.techtown.mymvvmtest
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class UserFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            UserFragment().apply {
+                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
