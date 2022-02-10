902384bea866cfd6de9cbba183cc360f1ddfa1d0
Olio <jgg0819@naver.com>
2022-02-11 01:06:57 +0900

    레트로핏 추가및 데이터 어댑터랑 연결

--- app/src/main/java/org/techtown/mymvvmtest/model/UserModel.kt
+++ app/src/main/java/org/techtown/mymvvmtest/model/UserModel.kt
@@ -1,23 +1,25 @@
 package org.techtown.mymvvmtest.model
 
+import com.google.gson.annotations.SerializedName
+
 data class UserModel(
-    val avatar_url: String,
-    val events_url: String,
-    val followers_url: String,
-    val following_url: String,
-    val gists_url: String,
-    val gravatar_id: String,
-    val html_url: String,
-    val id: Int,
-    val login: String,
-    val node_id: String,
-    val organizations_url: String,
-    val received_events_url: String,
-    val repos_url: String,
-    val score: Double,
-    val site_admin: Boolean,
-    val starred_url: String,
-    val subscriptions_url: String,
-    val type: String,
-    val url: String
+    @SerializedName("avatar_url") val avatar_url: String,
+    @SerializedName("events_url") val events_url: String,
+    @SerializedName("followers_url") val followers_url: String,
+    @SerializedName("following_url") val following_url: String,
+    @SerializedName("gists_url") val gists_url: String,
+    @SerializedName("gravatar_id") val gravatar_id: String,
+    @SerializedName("html_url") val html_url: String,
+    @SerializedName("id") val id: Int,
+    @SerializedName("login") val login: String,
+    @SerializedName("node_id") val node_id: String,
+    @SerializedName("organizations_url") val organizations_url: String,
+    @SerializedName("received_events_url") val received_events_url: String,
+    @SerializedName("repos_url") val repos_url: String,
+    @SerializedName("score") val score: Double,
+    @SerializedName("site_admin") val site_admin: Boolean,
+    @SerializedName("starred_url") val starred_url: String,
+    @SerializedName("subscriptions_url") val subscriptions_url: String,
+    @SerializedName("type") val type: String,
+    @SerializedName("url") val url: String
 )
