f2fa970f553ab70d9decc8de9763d805d33b6671
Olio <jgg0819@naver.com>
2022-02-12 23:47:25 +0900

    HW-0 스플래시 화면 카운트 세는 동작 추가
    - 3초카운트 세고 다음화면으로 넘어갈수있게 Timer추가

--- presentation/src/main/java/org/techtown/presentation/SplashActivity.kt
+++ presentation/src/main/java/org/techtown/presentation/SplashActivity.kt
@@ -3,22 +3,66 @@ package org.techtown.presentation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Toast
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import org.techtown.presentation.adapter.UserListAdapter
+import kotlinx.coroutines.*
+import org.techtown.presentation.databinding.ActivitySplashBinding
+import org.techtown.presentation.model.UserModel
 import org.techtown.presentation.model.UserRootModel
 import org.techtown.presentation.retorfit.RetrofitBuilder
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import kotlin.collections.ArrayList
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    //3초 카운트다운 세기위해서 추가.
+    private var count = 3
+    private var countDownTimer:CountDownTimer? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        //처음 들어갔을때 유저 목록 가져오기.
         getFirstUserInfo()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        //액티비티 없어지면 타이머도 취소 및 없애줌.
+        try{
+            countDownTimer!!.cancel()
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
+
+        countDownTimer = null
+
+    }
+
+    private fun countDownTimer(userList: ArrayList<UserModel>) {
+        countDownTimer = object : CountDownTimer(3000L, 1000L){
+            override fun onTick(p0: Long) {
+                binding.tvCount.text = "$count"
+                count --
+                if(count == 0){ //0일될떄는 3초가 다 지났으므로 다음화면으로 넘어가줍니다.
+                    val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                    intent.putExtra("user_list", userList)
+                    startActivity(intent)
+                }
+            }
+
+            override fun onFinish() {
+                binding.tvCount.text = "카운트종료"
+            }
+
+        }
     }
 
     private fun getFirstUserInfo() {
@@ -29,9 +73,8 @@ class SplashActivity : AppCompatActivity() {
 
                         val userList = response.body()!!.items
 
-                        val intent = Intent(this@SplashActivity, MainActivity::class.java)
-                        intent.putExtra("user_list", userList)
-                        startActivity(intent)
+                        countDownTimer(userList)
+                        countDownTimer!!.start()
 
                         Toast.makeText(this@SplashActivity, "데이터를 불러오는데 성공하였습니다." , Toast.LENGTH_LONG).show()
 
@@ -44,5 +87,11 @@ class SplashActivity : AppCompatActivity() {
                 Toast.makeText(this@SplashActivity, "데이터 불러오기 실패", Toast.LENGTH_LONG).show()
             }
         })
+    }
+
+    fun getCurrentTime(): String? {
+        val time = System.currentTimeMillis()
+        val dayTime = SimpleDateFormat("yyyy-mm-dd hh:mm:ss")
+        return dayTime.format(Date(time))
     }
 }
