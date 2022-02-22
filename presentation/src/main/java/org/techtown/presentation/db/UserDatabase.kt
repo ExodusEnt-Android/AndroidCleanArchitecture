725f8271a6d3b73135be90e6cf52f81137022869
Olio <jgg0819@naver.com>
2022-02-22 23:36:40 +0900

    HW-1 유저 DB 코드 포맷 및 버전 추가

--- presentation/src/main/java/org/techtown/presentation/db/UserDatabase.kt
+++ presentation/src/main/java/org/techtown/presentation/db/UserDatabase.kt
@@ -1,22 +1,27 @@
 package org.techtown.presentation.db
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import org.techtown.presentation.model.UserModel
 
+@Database(entities = [UserModel::class], version = 1)
 abstract class UserDatabase : RoomDatabase() {
-    abstract fun userDao() : UserDao
+    abstract fun userDao(): UserDao
 
-    companion object{
-        private var instance: UserDatabase ?=null
+    companion object {
+        private var instance: UserDatabase? = null
 
         @Synchronized
-        fun getInstance(context : Context?) : UserDatabase?{
-            if(instance == null){
-                synchronized(UserDatabase::class){
-                    instance = Room.databaseBuilder(context.applicationContext,
+        fun getInstance(context: Context?): UserDatabase? {
+            if (instance == null && context != null) {
+                synchronized(UserDatabase::class) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
                         UserDatabase::class.java,
-                        "user-database").build()
+                        "user-database"
+                    ).build()
                 }
             }
             return instance
