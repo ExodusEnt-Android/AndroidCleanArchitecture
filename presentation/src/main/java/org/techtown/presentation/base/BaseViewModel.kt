2baa1ad4cc8d0a508040830be73593fc3337b092
Olio <jgg0819@naver.com>
2022-03-11 09:27:44 +0900

    HW-2 BaseViewModel 생성
    - 공통 compositeDisposable protected로 선언

---
+++ presentation/src/main/java/org/techtown/presentation/base/BaseViewModel.kt
@@ -0,0 +1,14 @@
+package org.techtown.presentation.base
+
+import androidx.lifecycle.ViewModel
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+
+open class BaseViewModel : ViewModel() {
+    
+    protected val compositeDisposable = CompositeDisposable()
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+}
--- presentation/src/main/java/org/techtown/presentation/viewmodel/SplashViewModel.kt
+++ presentation/src/main/java/org/techtown/presentation/viewmodel/SplashViewModel.kt
@@ -6,18 +6,15 @@ import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.kotlin.addTo
 import io.reactivex.rxjava3.schedulers.Schedulers
 import io.reactivex.rxjava3.subjects.PublishSubject
+import org.techtown.presentation.base.BaseViewModel
 import org.techtown.presentation.model.UserModel
 import org.techtown.presentation.repository.UserRepository
 import org.techtown.presentation.util.Const
 
-class SplashViewModel(private val userRepository: UserRepository) : ViewModel() {
+class SplashViewModel(private val userRepository: UserRepository) : BaseViewModel() {
 
     val searchedUserPublishSubject: PublishSubject<List<UserModel>> =
         PublishSubject.create()
-
-
-    //여기서도 null체크 번거로움 제거.
-    private val compositeDisposable = CompositeDisposable()
 
     fun searchUser() {
         userRepository.getUserInfo("hello", 1, Const.PER_PAGE_LIST)
@@ -28,10 +25,5 @@ class SplashViewModel(private val userRepository: UserRepository) : ViewModel() 
             }, {
                 searchedUserPublishSubject.onError(Throwable("유저 검색중 문제가 생김."))
             }).addTo(compositeDisposable)
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        compositeDisposable.dispose()
     }
 }
