604d53495276c0b9ad27e149906d666edb7e4651
Olio <jgg0819@naver.com>
2022-03-13 23:32:29 +0900

    HW-2 SplashActivity 필요없는 코드 제거
    - timer도 viewmodel로 옮김 -> reteyWhen사용

--- presentation/src/main/java/org/techtown/presentation/activity/SplashActivity.kt
+++ presentation/src/main/java/org/techtown/presentation/activity/SplashActivity.kt
@@ -28,20 +28,8 @@ class SplashActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashBinding
 
-    //통신 성공 여부.
-    private var isSuccess:Boolean = false
-
-    //타이머 여러개돌면 안되므로 한번만 초기화하게 해줌.
-    private var countResponse : Boolean = false
-
     //처음 들어갈떄 임의용으로 정한 쿼리.
     private var firstQuery = "hello"
-
-    //os gc가발동할떄 프로세스가 죽어버리니까 single객체 가로채야됨.
-    private var compositeDisposable = CompositeDisposable()
-
-    //유저 리스트.
-    private var userList: ArrayList<UserModel>? = null
 
     //repository setting
     private val userRepository: UserRepository by lazy {
@@ -65,57 +53,29 @@ class SplashActivity : AppCompatActivity() {
 
         //데이터 Subscribe.
         getDataFromViewModel()
-
-        //타이머 시작.
-        excuteTimer()
+        splashViewModel.searchUser()
     }
-
-    private fun excuteTimer() = Observable.interval(1, 1, TimeUnit.SECONDS)
-        .observeOn(AndroidSchedulers.mainThread())
-        .take(7).subscribe { count ->
-
-            val countOfvalue = count.toInt()
-            binding.tvCount.text = "$countOfvalue"
-
-            //처음 들어갔을때 유저 목록 가져오기.
-            if (countOfvalue == 0) {
-                getFirstUserInfo()
-            }
-
-            if (isSuccess && userList != null) { //0일될떄는 3초가 다 지났으므로 다음화면으로 넘어가줍니다(최소 2초일때 넘어가지게하기).
-                val intent = Intent(this@SplashActivity, MainActivity::class.java)
-                intent.putParcelableArrayListExtra("user_list", userList)
-                intent.putExtra("first_query", firstQuery)
-                startActivity(intent)
-                finish()
-            } else if (countOfvalue % 3 == 0 && countOfvalue >= 3 && countOfvalue < 6 && !isSuccess) { //3초마다 재연결을 해줘야 되므로.
-                getFirstUserInfo()
-            } else if (countOfvalue % 3 == 0 && countOfvalue == 6 && !isSuccess) { //마지막까지 통신 안될시에는 사용자에게 알려주기.
-                binding.tvCount.text = "카운트종료"
-                getFirstUserInfo()
-                Toast.makeText(this@SplashActivity, "데이터 통신 실패하였습니다.", Toast.LENGTH_SHORT)
-                    .show()
-            }
-
-        }.addTo(compositeDisposable)
 
     private fun getDataFromViewModel() {
         splashViewModel.searchedUserPublishSubject.subscribe({
-            Util.closeProgress()
-            isSuccess = true
-            userList = it as ArrayList<UserModel>?
+            goMainActivity(it as ArrayList<UserModel>?)
         }, {
-            Util.closeProgress()
-            isSuccess = false
-            countResponse = true
-            Toast.makeText(this, "스플래시화면에서 가져오는데 실패하셨습니다.", Toast.LENGTH_SHORT)
+            Toast.makeText(this, it.message.toString(), Toast.LENGTH_SHORT)
                 .show()
+            finish()
         })
     }
 
-    private fun getFirstUserInfo() {
-        Util.showProgress(this@SplashActivity)
+    private fun goMainActivity(searchUsers: ArrayList<UserModel>?){
+        val intent = Intent(this@SplashActivity, MainActivity::class.java)
+        intent.putParcelableArrayListExtra(PARAM_USER_LIST, searchUsers)
+        intent.putExtra(PARAM_FIRST_QUERY, firstQuery)
+        startActivity(intent)
+        finish()
+    }
 
-        splashViewModel.searchUser()
+    companion object {
+        const val PARAM_USER_LIST = "user_list"
+        const val PARAM_FIRST_QUERY = "first_query"
     }
 }
--- presentation/src/main/java/org/techtown/presentation/viewmodel/SplashViewModel.kt
+++ presentation/src/main/java/org/techtown/presentation/viewmodel/SplashViewModel.kt
@@ -2,6 +2,7 @@ package org.techtown.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.kotlin.addTo
 import io.reactivex.rxjava3.schedulers.Schedulers
@@ -10,6 +11,7 @@ import org.techtown.presentation.base.BaseViewModel
 import org.techtown.presentation.model.UserModel
 import org.techtown.presentation.repository.UserRepository
 import org.techtown.presentation.util.Const
+import java.util.concurrent.TimeUnit
 
 class SplashViewModel(private val userRepository: UserRepository) : BaseViewModel() {
 
@@ -17,13 +19,23 @@ class SplashViewModel(private val userRepository: UserRepository) : BaseViewMode
         PublishSubject.create()
 
     fun searchUser() {
-        userRepository.getUserInfo("hello", 1, Const.PER_PAGE_LIST)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({ searchedUserList ->
-                searchedUserPublishSubject.onNext(searchedUserList.body()?.items)
-            }, {
-                searchedUserPublishSubject.onError(Throwable("유저 검색중 문제가 생김."))
-            }).addTo(compositeDisposable)
+        Single.zip(
+            userRepository.getUserInfo("hello", 1, Const.PER_PAGE_LIST)
+                .subscribeOn(Schedulers.io()).map {
+                    if (it.isSuccessful) {
+                        it.body()
+                    } else {
+                        throw Throwable()
+                    }
+                }.retryWhen { errorObservable ->
+                    errorObservable
+                        .delay(3, TimeUnit.SECONDS)
+                        .take(2)
+                }.observeOn(AndroidSchedulers.mainThread()),
+            Single.timer(2, TimeUnit.SECONDS), { dataModelSearchedUsers, time ->
+                searchedUserPublishSubject.onNext(dataModelSearchedUsers?.items)
+            }).onErrorReturn {
+            searchedUserPublishSubject.onError(Throwable("유저정보를 가져올수가 없습니다."))
+        }.subscribe().addTo(compositeDisposable)
     }
 }
