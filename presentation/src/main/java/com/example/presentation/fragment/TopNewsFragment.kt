f02fd8f759d11e092e0886f35daf9423cade06ff
이동훈 <dlwldlwldus123@gmail.com>
2022-10-23 20:56:06 +0900

    HW-1 -> Top news 데이터 계속 유지되게 수정

--- presentation/src/main/java/com/example/presentation/fragment/TopNewsFragment.kt
+++ presentation/src/main/java/com/example/presentation/fragment/TopNewsFragment.kt
@@ -1,6 +1,5 @@
 package com.example.presentation.fragment
 
-import android.os.Bundle
 import android.os.Parcelable
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,32 +14,40 @@ import com.example.presentation.retrofit.RetrofitHelper
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import timber.log.Timber
 
-class TopNewsFragment:BaseFragment<FragmentTopNewsBinding>(R.layout.fragment_top_news) {
+class TopNewsFragment : BaseFragment<FragmentTopNewsBinding>(R.layout.fragment_top_news) {
 
     lateinit var topNewsListAdapter: TopNewsListAdapter
     private var totalResult = DEFAULT_LIST_SIZE
     private var page = 1
-
+    private var rcyScrollLState: Parcelable? = null
+    private var isAlreadyInitialized = false
+    private val topNewsList = mutableListOf<Article>()
 
     override fun FragmentTopNewsBinding.onCreateView() {
         initSet()
         setListenerEvent()
         setToolbar()
-        getTopNewsList()
     }
 
-    private fun initSet(){
-        page = 1
+    private fun initSet() {
         topNewsListAdapter = TopNewsListAdapter()
         binding.rvTopNewsList.apply {
             adapter = topNewsListAdapter
         }
+
+        if (!isAlreadyInitialized) {
+            isAlreadyInitialized = true
+            //탑 뉴스기사 리스트 가져오기
+            getTopNewsList()
+        } else {
+            topNewsListAdapter.submitList(topNewsList)
+            binding.rvTopNewsList.layoutManager?.onRestoreInstanceState(rcyScrollLState)
+        }
     }
 
     //리스너 이벤트 모음
-    private fun setListenerEvent(){
+    private fun setListenerEvent() {
         topNewsListAdapter.setOnTopNewsItemClickListener(object :
             TopNewsListAdapter.ItemClickListener {
             override fun onTopNewItemClick(article: Article) {
@@ -49,7 +56,7 @@ class TopNewsFragment:BaseFragment<FragmentTopNewsBinding>(R.layout.fragment_top
             }
         })
 
-        binding.rvTopNewsList.addOnScrollListener(object :RecyclerView.OnScrollListener(){
+        binding.rvTopNewsList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
 
@@ -57,8 +64,11 @@ class TopNewsFragment:BaseFragment<FragmentTopNewsBinding>(R.layout.fragment_top
                 val lastVisiblePosition =
                     (binding.rvTopNewsList.layoutManager as LinearLayoutManager).findLastVisibleItemPosition()
 
-                val lastPosition = topNewsListAdapter.itemCount-1
+                val lastPosition = topNewsListAdapter.itemCount - 1
 
+                //리사이클러뷰 스크롤 위치 뷰모델에 캐싱 -> 유지하기 위해서
+                rcyScrollLState =
+                    binding.rvTopNewsList.layoutManager?.onSaveInstanceState()
 
                 // 스크롤이 끝에 도달했는지 확인후  api를 요청해서 다음 페이지를 받아온다.
                 if (!recyclerView.canScrollVertically(1)
@@ -71,20 +81,20 @@ class TopNewsFragment:BaseFragment<FragmentTopNewsBinding>(R.layout.fragment_top
     }
 
     //toolbar setting
-    private fun setToolbar(){
+    private fun setToolbar() {
         binding.toolbar.tvTitle.text = requireActivity().getString(R.string.top_news)
     }
 
     //탑 뉴스 리스트 가져오기
-    private fun getTopNewsList(){
+    private fun getTopNewsList() {
 
         //전체 reesult 값이  현재 뉴스 리스트 값과 같거나 작으면, 페이징  처리 마감. 해줌.
-        if(totalResult != DEFAULT_LIST_SIZE && totalResult <= topNewsListAdapter.currentList.size){
+        if (totalResult != DEFAULT_LIST_SIZE && totalResult <= topNewsListAdapter.currentList.size) {
             return
         }
 
         RetrofitHelper.apiService.getTopHeadLines(page = page, pageSize = Const.PageSize)
-            .enqueue(object : Callback<BaseDataModel<Article>>{
+            .enqueue(object : Callback<BaseDataModel<Article>> {
                 override fun onResponse(
                     call: Call<BaseDataModel<Article>>,
                     response: Response<BaseDataModel<Article>>
@@ -93,25 +103,29 @@ class TopNewsFragment:BaseFragment<FragmentTopNewsBinding>(R.layout.fragment_top
                     val result = response.body()
 
                     //새 뉴스 게시글리스트
-                    totalResult = result?.totalResults?:0
+                    totalResult = result?.totalResults ?: 0
                     val newTopNewsArticleList = result?.articles
-                    val currentArticleList = topNewsListAdapter.currentList.toMutableList()
 
                     if (newTopNewsArticleList != null) {
-                        currentArticleList.addAll(newTopNewsArticleList)
+                        topNewsList.addAll(newTopNewsArticleList)
                     }
 
-                    page ++
-                    topNewsListAdapter.submitList(currentArticleList)
+                    page++
+                    topNewsListAdapter.submitList(topNewsList)
+
+                    //기존  스크롤  위치 정보 캐싱되어있으면 다시 적용 해줌.
+                    if (rcyScrollLState != null) {
+                        binding.rvTopNewsList.layoutManager?.onRestoreInstanceState(rcyScrollLState)
+                    }
                 }
 
                 override fun onFailure(call: Call<BaseDataModel<Article>>, t: Throwable) {
-                   showToast(t.message.toString())
+                    showToast(t.message.toString())
                 }
             })
     }
 
-    companion object{
+    companion object {
         const val DEFAULT_LIST_SIZE = -1
     }
 }
