50ce6e67a85b94a059b00f02166a541a9544ea09
mingue <mingue0605@myloveidol.com>
2023-01-18 21:03:44 +0900

    NewsListAdapter도 binding 적용

--- presentation/src/main/java/com/example/presentation/Adapter/NewsListAdapter.kt
+++ presentation/src/main/java/com/example/presentation/Adapter/NewsListAdapter.kt
@@ -8,29 +8,36 @@ import androidx.annotation.NonNull
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.presentation.Articles
 import com.example.presentation.R
+import com.example.presentation.databinding.NewsItemBinding
 import java.util.ArrayList
 
 class NewsListAdapter (
     private val context: Context,
     private var onClickListener: OnClickListener
-)  : RecyclerView.Adapter<NewsListAdapter.ViewHolder>() {
+)  : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private var newsData = ArrayList<Articles>()
 
     interface OnClickListener{
         fun onItemClicked(item : Articles, view: View)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.news_item,parent,false)
-        return ViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view : NewsItemBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.news_item,
+            parent,
+            false
+        )
+        return NewsListViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(newsData[position])
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as NewsListViewHolder).bind(newsData[position])
     }
 
     override fun getItemCount(): Int = newsData.size
@@ -41,21 +48,16 @@ class NewsListAdapter (
         notifyDataSetChanged()
     }
 
-    inner class ViewHolder(view:View) : RecyclerView.ViewHolder(view){
-
-        private val ivPhoto : AppCompatImageView = itemView.findViewById(R.id.iv_photo)
-        private val tvTitle : AppCompatTextView = itemView.findViewById(R.id.tv_title)
-        private val tvAuthor : AppCompatTextView = itemView.findViewById(R.id.tv_author)
-        private val clArticle : ConstraintLayout = itemView.findViewById(R.id.cl_article)
+    inner class NewsListViewHolder(val binding: NewsItemBinding) : RecyclerView.ViewHolder(binding.root){
 
         fun bind(item : Articles){
-            Glide.with(itemView).load(item.urlToImage).into(ivPhoto)
-            tvTitle.text = item.title
-            tvAuthor.text = item.author
+            Glide.with(itemView).load(item.urlToImage).into(binding.ivPhoto)
+            binding.tvTitle.text = item.title
+            binding.tvAuthor.text = item.author
 
             val listener = View.OnClickListener { view ->
                 onClickListener.onItemClicked(item, view) }
-            clArticle.setOnClickListener(listener)
+            binding.clArticle.setOnClickListener(listener)
         }
     }
 }
